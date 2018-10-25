9479f6597b9b27e8694c19f02cf42745ea549d30
Tomasz Magdziarz <tljm@wp.pl>
2018-10-25 18:31:01 +0200

    options and auto agrs generation

--- src/apps/mdjpp.py
+++ src/apps/mdjpp.py
@@ -1,3 +1,4 @@
+from jpp.options import default_options
 from jpp.parse import JournalParser
 
 if __name__ == "__main__":
@@ -10,6 +11,15 @@ if __name__ == "__main__":
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("-j", action="store", dest="journal_file", required=True, help="Config file filename.")
+    
+    for option in (o for o in dir(default_options) if o.startswith('jpp_')):
+        name = option[4:]
+        parser.add_argument("--%s" % name,
+                            action="store",
+                            dest=name,
+                            required=False,
+                            default=getattr(default_options,option),
+                            type=type(getattr(default_options,option)))
     
     args = parser.parse_args()
 
---
+++ src/jpp/options.py
@@ -0,0 +1,11 @@
+
+class TagsFilters(object):
+    
+    jpp_skip_notag_content = False
+    jpp_skip_global_tag_content = False
+
+class JPP(TagsFilters):
+    pass
+    
+default_options = JPP()
+
