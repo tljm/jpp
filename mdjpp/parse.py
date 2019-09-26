74b7373514e489e251106a04ff61df8730c056f0
Tomasz Magdziarz <tljm@wp.pl>
2019-09-26 20:47:06 +0200

    time tags are parsed

--- mdjpp/parse.py
+++ mdjpp/parse.py
@@ -1,7 +1,20 @@
 from mdjpp.tags import istag, maketag
-from mdjpp.tags import Tag,Date,Verbatim,NoTag,Multi,Global
+from mdjpp.tags import Tag,Date,Verbatim,NoTag,Multi,Global,Time
 from mdjpp.render import sstr,Null,HTML,MD
 from mdjpp.options import default_options
+
+# Time tags handling:
+#
+# Date tag:
+# 
+# Each Date tag has begin and end time. First Time tag in given Date tag is
+# begin and last Time tag is end. If there is only one Time tag then it is end.
+# Within given Date tag we can have current Time.
+#
+# Multi tag:
+# 
+# 
+
 
 
 ################################################################################
@@ -23,11 +36,19 @@ class JournalParser(object):
             self.engine = MD()
         elif default_options.mdjpp_html:
             self.engine = HTML()
-            
+        
+        self.time_queue = []
     
     def __del__(self):
         self.finalize()
     
+    @property
+    def mtag(self):
+        """
+        :return: Metastable tag.
+        """
+        return self.metastable_tag
+   
     @property
     def otag(self):
         """
@@ -35,10 +56,99 @@ class JournalParser(object):
         """
         return self.open_tags[-1]
         
+    @property
+    def dtag(self):
+        """
+        :return: Current Date tag.
+        """
+        if isinstance(self.metastable_tag,Date):
+            return self.metastable_tag
+        for tag in self.open_tags[::-1]:
+            if isinstance(tag,Date):
+                return tag
+    
+    @property
+    def ctag(self):
+        """
+        :return: Current tag.
+        """
+        if self.mtag is not None:
+            return self.mtag
+        if isinstance(self.otag,Date):
+            if self.otag.last_tag is not None:
+                return self.otag.last_tag
+        return self.otag
+        
+    @property
+    def ptag(self):
+        """
+        :return: Previous tag.
+        """
+        if self.mtag == self.otag:
+            if self.dtag:
+                if self.dtag.last_tag is not None:
+                    return self.dtag.last_tag
+        else:
+            tagnr = 0
+            if self.mtag is not None:
+                tagnr += 1
+            if isinstance(self.otag,Date):
+                if self.otag.last_tag is not None:
+                    tagnr += 1
+                    if tagnr == 2:
+                        return self.otag.last_tag
+            tagnr += 1
+            if tagnr == 2:
+                return self.otag
+            if len(self.open_tags) > 1:
+                return self.open_tags[-2]
+
+    
+    def time_annotate_date(self,ttag):
+        # Date tags
+        if self.dtag:
+            self.dtag.add_time(ttag)
+            # add tag to the queue
+            self.dtag.time_queue.append(ttag)
+            self.time_annotate()
+
+    def time_annotate(self):
+        # Other tags (no Date tags) but Date tag was used
+        if self.dtag:
+            if not isinstance(self.ctag,Date):
+                # if ctag is empty and ptag is not Date and has end it can be used as begin of ctag
+                if self.ctag.isempty and self.ptag and not isinstance(self.ptag,Date) and self.ptag.end:
+                    self.ctag.add_time(self.ptag.end)
+                    print('empty',self.ctag)
+                    print('  mtag',self.mtag)
+                    print('  ptag',self.ptag)
+                    print('  otag',self.otag)
+                    print('  dtag',self.dtag)
+                    print('  dtag.last',self.dtag.last_tag)
+                # last from queue might go to the previous tag if current tag is empty and has no begin
+                if not isinstance(self.ptag,Date) and self.ctag.begin is None and self.ctag.isempty and self.ptag:
+                    if len(self.dtag.time_queue):
+                        print('from Q, -1')
+                        self.ptag.add_time(self.dtag.time_queue[-1])
+                # last from queue goes to current tag
+                if len(self.dtag.time_queue):
+                    print('from Q, pop')
+                    self.ctag.add_time(self.dtag.time_queue.pop())
+            
+        
+        
+    
+        
     def printable(self):
         # is current state printable?
         return True
     
+    def printme_text(self,line):
+        """
+        Prints input line by submitting it to printme_text method of open tag.
+        """
+        self.otag.printme_text(line)
+
     def printme(self,line):
         """
         Prints input line by submitting it to printme method of open tag.
@@ -55,6 +165,8 @@ class JournalParser(object):
             tag = Global(tag)
         # adds tag to open_tag list
         self.open_tags.append(tag)
+        # time annotate
+        self.time_annotate()
         # print tag opening if current state is printable
         if self.printable():
             # prints tag opener
@@ -92,20 +204,26 @@ class JournalParser(object):
         """
         Proceeds with source, line by line.
         """
+        
         for line in source:
             line = line.rstrip()
+            if istag(line) and isinstance(maketag(line),Time): # here, handle Time tags
+                self.time_annotate_date(maketag(line)) # annotate Date tag and queue Time tag
+                if not default_options.propagate_time:
+                    continue
             # not a tag!
             if not istag(line):
-                if self.metastable_tag:
+                if self.metastable_tag and len(line):
                     # open metastable tag
                     self.open_tag(self.metastable_tag)
                     self.metastable_tag = None
                 # print line
                 if self.printable():
-                    self.printme(line)
+                    self.printme_text(line)
             # tag!
             else:
-                this_tag = maketag(line) # here, handle Time tags
+                # make regular tag
+                this_tag = maketag(line)
                 # no metastable tag
                 if not self.metastable_tag:
                     # make new metastable tag
@@ -133,6 +251,7 @@ class JournalParser(object):
             self.final_printout(self.open_tags.pop())
     
     def final_printout(self,tag):
+        print(self.engine.tag_time(tag))
         for line in tag.body:
             if isinstance(line,Tag):
                 self.final_printout(line)
